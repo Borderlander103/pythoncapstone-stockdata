17b06cde274a138b1d1f2a7cfe825380483ab719
Brian Richardson <brichardson@catalyte.io>
2021-01-27 08:16:29 -0800

    Add methods to data_loader

--- stockdata/data_loader.py
+++ stockdata/data_loader.py
@@ -12,6 +12,34 @@ stocks = []
 class Data_Loader:
     def __init__(self):
         print("hello world")
+        import_csv()
+        clean_data()
+        convert_dates()
+        populate_stocks()
+
+    def import_csv():
+        with open(importfile, 'r') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        next(csvreader)
+        for row in csvreader:
+            rows.append(row)
+
+    def clean_data():
+        for row in rows:
+            for index, col in enumerate(row):
+                if col == '':
+                    row[index] = None
+
+    def convert_dates():
+        for index, row in enumerate(rows):
+            # index 7 is the date column
+            if row[7] == None:
+                continue
+            rows[index][7] = str(dateutil.parser.parse(row[7]).date())
+
+    def populate_stocks()
+    for row in rows:
+            stocks.append(StockData(row))
 
 
 x = Data_Loader()
