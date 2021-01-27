7ad886061190e265542f8d764d1a9a3abf8fb99d
Brian Richardson <brichardson@catalyte.io>
2021-01-27 12:51:30 -0800

    Creaate analysis_utils with placeholder methods

---
+++ stockdata/analysis_utils.py
@@ -0,0 +1,13 @@
+class Analysis_Utils:
+
+    def max_min(self):
+        pass
+
+    def average(self):
+        pass
+
+    def median(self):
+        pass
+
+    def conver_currency(self, new_currency):
+        pass
