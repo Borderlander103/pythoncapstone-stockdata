c69743206a1e885f9edb0ca6adcb86ad09fae1cb
Brian Richardson <brichardson@catalyte.io>
2021-01-27 07:38:45 -0800

    Add base class for data loader

--- stockdata/data_loader.py
+++ stockdata/data_loader.py
@@ -1,9 +1,17 @@
 import csv
 import dateutil.parser
 from pathlib import Path
-from .stock_model import StockData
+from stock_model import StockData
 
 data_folder = Path("resources/")
 csv_path = data_folder / 'stocks.csv'
 rows = []
 stocks = []
+
+
+class Data_Loader:
+    def __init__(self):
+        print("hello world")
+
+
+x = Data_Loader()
