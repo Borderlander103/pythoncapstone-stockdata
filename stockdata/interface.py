bd1cbc66b985c153d7907798c717b75de1ec8928
Brian Richardson <brichardson@catalyte.io>
2021-01-28 09:40:01 -0800

    Fix variable scope bug for options

--- stockdata/interface.py
+++ stockdata/interface.py
@@ -3,7 +3,7 @@ import time
 
 class Interface:
 
-    # options = ["name", "industry", "market"]
+    options = ["name", "industry", "market"]
 
     def welcome(self):
         print("\n\n\n\n\n\n\n    --- Stock Data Reports --- \n")
@@ -11,13 +11,16 @@ class Interface:
         print("imported stock data.\n")
 
     def main_menu(self):
-        options = ["name", "industry", "market"]
+        # options = ["name", "industry", "market"]
         print("Please choose from the following options (enter only \nthe number):\n")
         print("1. Lookup by name")
         print("2. Lookup by industry")
         print("3. Lookup by market\n")
-        choice = options[int(input()) - 1]
+        choice = self.options[int(input()) - 1]
         print(f"\nOkay, looking up by {choice}.")
         time.sleep(3)
         print("Give me a second...")
         time.sleep(3)
+
+    def main_validation(self):
+        pass
