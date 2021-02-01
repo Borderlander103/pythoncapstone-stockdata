bdeacfc0ead52991e8a854f8cec18e1b84d6a99f
Brian Richardson <brichardson@catalyte.io>
2021-02-01 07:03:14 -0800

    Fill out the three main analysis methods

--- stockdata/analysis_utils.py
+++ stockdata/analysis_utils.py
@@ -1,13 +1,16 @@
+import statistics
+
+
 class Analysis_Utils:
 
-    def max_min(self):
-        pass
+    def max_min(self, values):
+        return (max(values), min(values))
 
-    def average(self):
-        pass
+    def average(self, values):
+        return sum(values)/len(values)
 
-    def median(self):
-        pass
+    def median(self, values):
+        return statistics.median(values)
 
     def conver_currency(self, new_currency):
         pass
