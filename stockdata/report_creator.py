45d60d381b60035275e652460efd5b4c4274f96e
Brian Richardson <brichardson@catalyte.io>
2021-02-02 07:23:00 -0800

    Fix class import in report_creator

--- stockdata/report_creator.py
+++ stockdata/report_creator.py
@@ -1,10 +1,14 @@
 import settings
+from analysis_utils import Analysis_Utils
 
 
 class Report_Creator(Analysis_Utils):
 
-    def __init__(self):
-        Analysis_Utils.__init__(self)
+    def __init__(self) -> None:
+        super().__init__()
+
+    # def __init__(self):
+    #     Analysis_Utils.__init__(self)
 
     def generate_report(self):
         print(settings.stock_values, settings.key,
