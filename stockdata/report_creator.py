999ca15a290e98c6639af54444d65fdb15db0653
Brian Richardson <brichardson@catalyte.io>
2021-02-02 10:04:35 -0800

    Update formatting and seperate min and max values

--- stockdata/report_creator.py
+++ stockdata/report_creator.py
@@ -7,16 +7,18 @@ class Report_Creator(Analysis_Utils):
 
     def __init__(self, ) -> None:
         super().__init__()
-        self.headline = f"\nStock Report for the {settings.key.title()} {settings.value}:\n"
+        self.headline = f"\n\n\nStock Report for the {settings.key.title()} '{settings.value}':\n"
         self.average_val = f"Average Value: {self.average(settings.stock_values)}"
         self.median_val = f"Median Value: {self.median(settings.stock_values)}"
-        self.max_min_val = f"Maximum and Minimum Values: {self.max_min(settings.stock_values)}"
+        self.max_val = f"Maximum Value: {self.max_min(settings.stock_values)[0]}"
+        self.min_val = f"Minimum Value: {self.max_min(settings.stock_values)[1]}"
 
     def print_report(self):
         print(self.headline)
         print(self.average_val)
         print(self.median_val)
-        print(self.max_min_val)
+        print(self.max_val)
+        print(self.min_val)
         # print(settings.key,
         #       settings.value, settings.currency)
         # print((max(settings.stock_values), min(settings.stock_values)))
