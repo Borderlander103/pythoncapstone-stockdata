93f9a90293d93a03502d7c9c574b61000d4dd90a
Brian Richardson <brichardson@catalyte.io>
2021-02-02 10:27:28 -0800

    Format report to use currency.

--- stockdata/report_creator.py
+++ stockdata/report_creator.py
@@ -8,17 +8,21 @@ class Report_Creator(Analysis_Utils):
     def __init__(self, ) -> None:
         super().__init__()
         self.headline = f"\n\n\nStock Report for the {settings.key.title()} '{settings.value}':\n"
-        self.average_val = f"Average Value: {self.average(settings.stock_values)}"
-        self.median_val = f"Median Value: {self.median(settings.stock_values)}"
-        self.max_val = f"Maximum Value: {self.max_min(settings.stock_values)[0]}"
-        self.min_val = f"Minimum Value: {self.max_min(settings.stock_values)[1]}"
+        self.average_val = "Average Value: ${:,.2f}".format(
+            self.average(settings.stock_values), 2)
+        self.median_val = "Median Value: ${:,.2f}".format(
+            self.median(settings.stock_values), 2)
+        self.min_val = "Minimum Value: ${:,.2f}".format(
+            self.max_min(settings.stock_values)[1])
+        self.max_val = "Maximum Value: ${:,.2f}".format(
+            self.max_min(settings.stock_values)[0], 2)
 
     def print_report(self):
         print(self.headline)
         print(self.average_val)
         print(self.median_val)
-        print(self.max_val)
         print(self.min_val)
+        print(self.max_val)
         # print(settings.key,
         #       settings.value, settings.currency)
         # print((max(settings.stock_values), min(settings.stock_values)))
